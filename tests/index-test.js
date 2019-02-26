0ccfa9f7ee898763fce24645719307c630052746
Casper Engelmann <me@casperengelmann.com>
2019-02-26 22:50:16 +0100

    added tests

--- tests/index-test.js
+++ tests/index-test.js
@@ -1,8 +1,16 @@
 import expect from 'expect'
 import React from 'react'
-import {render, unmountComponentAtNode} from 'react-dom'
+import { render, unmountComponentAtNode } from 'react-dom'
 
 import Component from 'src/'
+
+function Example(props) {
+  return (
+    <Component {...props}>
+      <h1>Hello, world!</h1>
+    </Component>
+  )
+}
 
 describe('Component', () => {
   let node
@@ -15,9 +23,21 @@ describe('Component', () => {
     unmountComponentAtNode(node)
   })
 
-  it('displays a welcome message', () => {
-    render(<Component/>, node, () => {
-      expect(node.innerHTML).toContain('Welcome to React components')
+  it('accepts jsx children to be inserted inside container', () => {
+    render(<Example />, node, () => {
+      expect(node.getElementsByTagName('h1').length).toBeGreaterThan(0)
+    })
+  })
+
+  it('outputs the correct message in the title', () => {
+    render(<Example />, node, () => {
+      expect(node.querySelector('.reveal-title h1').textContent).toContain('Hello, world!')
+    })
+  })
+
+  it('sets classes correctly on title container', () => {
+    render(<Example className="testing" />, node, () => {
+      expect(node.querySelector('.reveal-title').classList.contains('testing')).toBeTruthy()
     })
   })
 })
