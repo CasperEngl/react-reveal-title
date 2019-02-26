d998cb63b3ba1623f2d05fc4dc72ece59140918b
Casper Engelmann <me@casperengelmann.com>
2019-02-26 23:27:53 +0100

    only initialize ScrollMagic and GSAP if window is defined

--- src/index.js
+++ src/index.js
@@ -33,43 +33,45 @@ const RevealTitle = ({
   const revealer = React.useRef(null);
   const title = React.useRef(null);
 
-  const revealTimeline = new TimelineMax({
-    delay: 0.5,
-    paused: true,
-  });
-
-  const controller = new ScrollMagic.Controller();
-
-  const onEnter = () => {
-    revealTimeline.play();
-  };
-
-  useEffect(() => {
-    new ScrollMagic.Scene({
-      triggerHook: 'onEnter',
-      triggerElement: revealer.current,
-      offset: 300,
-      duration: '100%',
-    })
-      .on('enter', onEnter)
-      .addTo(controller);
-
-    revealTimeline
-      .to(revealer.current, 0.25, {
-        delay,
-        scaleX: 1,
-        ease: Expo.easeOut,
+  if (typeof window !== 'undefined') {
+    const revealTimeline = new TimelineMax({
+      delay: 0.5,
+      paused: true,
+    });
+  
+    const controller = new ScrollMagic.Controller();
+  
+    const onEnter = () => {
+      revealTimeline.play();
+    };
+  
+    useEffect(() => {
+      new ScrollMagic.Scene({
+        triggerHook: 'onEnter',
+        triggerElement: revealer.current,
+        offset: 300,
+        duration: '100%',
       })
-      .to(revealer.current, 0.45, {
-        scaleX: 0,
-        transformOrigin: 'right',
-        ease: Expo.easeOut,
-      })
-      .to(title.current, 0, {
-        visibility: 'visible',
-        ease: Expo.easeOut,
-      }, '=-.45');
-  }, []);
+        .on('enter', onEnter)
+        .addTo(controller);
+  
+      revealTimeline
+        .to(revealer.current, 0.25, {
+          delay,
+          scaleX: 1,
+          ease: Expo.easeOut,
+        })
+        .to(revealer.current, 0.45, {
+          scaleX: 0,
+          transformOrigin: 'right',
+          ease: Expo.easeOut,
+        })
+        .to(title.current, 0, {
+          visibility: 'visible',
+          ease: Expo.easeOut,
+        }, '=-.45');
+    }, []);
+  }
 
   return (
     <TitleContainer className={`reveal-title ${className}`} {...props}>
