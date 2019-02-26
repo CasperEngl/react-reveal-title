a28450a735b031db2dc785f0715b2e586331babb
Casper Engelmann <me@casperengelmann.com>
2019-02-26 22:44:47 +0100

    Added example

--- demo/src/index.js
+++ demo/src/index.js
@@ -6,8 +6,10 @@ import Example from '../../src'
 class Demo extends Component {
   render() {
     return <div>
-      <h1>react-reveal-title Demo</h1>
-      <Example/>
+      <h1>React Reveal Title Demo</h1>
+      <Example style={{ display: 'inline-block' }}>
+        <h1 style={{ margin: '0' }}>Hello, world!</h1>
+      </Example>
     </div>
   }
 }
